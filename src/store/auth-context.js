cfe513a0632ba7c634dbb66ee329ce9f141bf068
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-09-30 22:32:27 +0200

    final

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useCallback, useEffect, useState } from "react";
+let logoutTimer;
 const AuthContext = React.createContext({
   token: null,
   isLoggedIn: false,
@@ -14,21 +14,47 @@ const calculateRemainingTime = (expirationTime) => {
   return remainingDuration;
 };
 
+const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem("token");
+  const storedExpirationTime = localStorage.getItem("expirationTime");
+  const remainingTime = calculateRemainingTime(storedExpirationTime);
+
+  if (remainingTime <= 60 * 1000) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTime");
+    return null;
+  }
+  return {
+    token: storedToken,
+    duration: storedExpirationTime,
+  };
+};
+
 export const AuthContextProvider = ({ children }) => {
-  const initialToken = localStorage.getItem("token");
+  const tokenData = retrieveStoredToken();
+  let initialToken;
+  if (tokenData) {
+    initialToken = tokenData.token;
+  }
   const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null);
     localStorage.removeItem("token");
-  };
+    localStorage.removeItem("expirationTime");
+    if (logoutTimer) clearTimeout(logoutTimer);
+  }, []);
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem("token", token);
+    localStorage.setItem("expirationTime", expirationTime);
     const remainingTime = calculateRemainingTime(expirationTime);
-    setTimeout(logoutHandler, remainingTime);
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
+  useEffect(() => {
+    if (tokenData) logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+  }, [tokenData, logoutHandler]);
   const contextValue = {
     token,
     isLoggedIn: userIsLoggedIn,
